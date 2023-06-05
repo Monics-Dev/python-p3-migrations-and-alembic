77c436a6dda7957fc1e444f6ea0f5e0a61ca8bf8
Monica <monijerobon@gmail.com>
2023-06-05 19:46:46 +0300

    complete

---
+++ app/migrations/versions/2c3f06f0eb03_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2c3f06f0eb03
+Revises: 
+Create Date: 2023-06-05 19:33:32.955044
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2c3f06f0eb03'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
